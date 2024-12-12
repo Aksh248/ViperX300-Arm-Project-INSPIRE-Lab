b1406aa4e60afe6e624684383cd69e3775ea2b58
Aksh248 <108886286+Aksh248@users.noreply.github.com>
2024-12-12 18:24:05 +0530

    Delete Final pipeline directory

--- Final pipeline/arm_motion.py
+++
@@ -1,8 +0,0 @@
-from interbotix_xs_modules.arm import InterbotixManipulatorXS
-import math
-
-bot = InterbotixManipulatorXS("vx300s", "arm", "gripper")
-
-bot.arm.go_to_home_pose()
-bot.arm.go_to_sleep_pose()
-
--- Final pipeline/recorder_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- 
-while true; do
-python3 recorder.py
-echo "A new file appeared!"
-python3 Speech_pipe.py
-python3 ans.py
-    
-done
